c8977cce7bc506f3f2e2bf5e61f4b37d31e1b1d3
trungquantrangnguyen <tranntquan@gmail.com>
2023-04-25 12:15:35 +0700

    add Insertion Sort Visualizer

--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -71,6 +71,42 @@ const selectionSort = async () => {
   }
 }
 
+// Insertion Sort
+const insertionSort = async () => {
+  let currentArr = primaryArray
+  let sorted = false
+  setAlgorithm({name: 'Insertion Sort', timeComplexity: 'O(n^2)'})
+
+  while (!sorted) {
+      sorted = true
+
+      for (let i = 1; i < currentArr.length; i++) {
+          let current = currentArr[i]
+          let j = i - 1
+          while (j >= 0 && currentArr[j] > current) {
+              currentArr[j + 1] = currentArr[j]
+              setPrimaryArray([...primaryArray, currentArr])
+
+              let bar1 = document.getElementById(j + 1).style
+              let bar2 = document.getElementById(j).style
+              bar1.backgroundColor = '#DC143C'
+              bar2.backgroundColor = '#6A5ACD'
+
+              await sleep(animationSpeed)
+
+              bar1.backgroundColor = '#FF7F50'
+              bar2.backgroundColor = '#FF7F50'
+
+              j--
+              sorted = false
+          }
+          currentArr[j + 1] = current
+          setPrimaryArray([...primaryArray, currentArr])
+      }
+      if (sorted) finishedAnimation()
+  }
+}
+
 
   return <></>
 }
