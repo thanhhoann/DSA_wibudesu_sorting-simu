d6de02cf7e86e99442d79be2fe9fff9b67a5f8cb
trungquantrangnguyen <tranntquan@gmail.com>
2023-04-25 12:21:28 +0700

    Add Quick Sort Visualizer

--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -202,5 +202,58 @@ const merge = async (arr, low, mid, high) => {
   }
 }
 
+// Quick Sort
+const quickSort = async () => {
+  setAlgorithm({name: 'Quick Sort', timeComplexity: 'O(n log(n))'})
+  let currentArr = primaryArray
+
+  await sorts(currentArr, 0, currentArr.length - 1)
+  finishedAnimation()
+}
+
+const sorts = async (arr, left, right) => {
+  if (left < right) {
+      let partitionIndex = partition(arr, left, right)
+
+      setPrimaryArray([...primaryArray, arr])
+      await sleep(animationSpeed)
+
+      await sorts(arr, left, partitionIndex - 1)
+      await sorts(arr, partitionIndex + 1, right)
+  }
+}
+
+const partition = (arr, left, right) => {
+  let pivot = arr[right]
+  let i = left - 1
+
+  for (let j = left; j < right; j++) {
+      if (arr[j] < pivot) {
+          i++
+          let temp = arr[i]
+          arr[i] = arr[j]
+          arr[j] = temp
+
+          let bar1 = document.getElementById(i).style
+          let bar2 = document.getElementById(j).style
+          bar1.backgroundColor = '#DC143C'
+          bar2.backgroundColor = '#6A5ACD'
+
+          setTimeout(() => {
+              bar1.backgroundColor = '#ff7f50'
+              bar2.backgroundColor = '#ff7f50'
+          }, 200)
+
+          setPrimaryArray([...primaryArray, arr])
+      }
+  }
+
+  let temp = arr[i + 1]
+  arr[i + 1] = arr[right]
+  arr[right] = temp
+
+  return i + 1
+}
+
   return <></>
 }
