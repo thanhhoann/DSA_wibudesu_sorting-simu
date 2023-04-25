27edc8ed9ec94098f22c17686c3b8fd448991a00
trungquantrangnguyen <tranntquan@gmail.com>
2023-04-25 12:13:15 +0700

    Add Selection Sort Visuallizer

--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -35,6 +35,42 @@ const bubbleSort = async () => {
       if (sorted) finishedAnimation()
   }
 }
+// Selection Sort
+const selectionSort = async () => {
+  let currentArr = primaryArray
+  let sorted = false
+  setAlgorithm({name: 'Selection Sort', timeComplexity: 'O(n^2)'})
+
+  while (!sorted) {
+      sorted = true
+
+      for (let i = 0; i < currentArr.length - 1; i++) {
+          for (let j = i + 1; j < currentArr.length; j++) {
+              if (currentArr[i] > currentArr[j]) {
+                  let swap1 = currentArr[i]
+                  let swap2 = currentArr[j]
+                  currentArr[i] = swap2
+                  currentArr[j] = swap1
+                  setPrimaryArray([...primaryArray, currentArr])
+
+                  let bar1 = document.getElementById(i).style
+                  let bar2 = document.getElementById(j).style
+                  bar1.backgroundColor = '#DC143C'
+                  bar2.backgroundColor = '#6A5ACD'
+
+                  await sleep(animationSpeed)
+
+                  bar1.backgroundColor = '#FF7F50'
+                  bar2.backgroundColor = '#FF7F50'
+
+                  sorted = false
+              }
+          }
+      }
+      if (sorted) finishedAnimation()
+  }
+}
+
 
   return <></>
 }
